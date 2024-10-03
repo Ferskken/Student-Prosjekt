43e766034bcb079ef8e726037f3c2d166e44beb4
ferskken <andreashavnen@gmail.com>
2024-10-03 14:40:22 +0200

    Added Swagger XML comments in bookModel

--- backend/BookModel.cs
+++ backend/BookModel.cs
@@ -5,28 +5,47 @@ namespace DefaultNamespace
 {
     public class BookModel
     {
-        [Key] 
-        public int Id { get; set; } 
+	/// <summary>
+    /// The unique identifier for the book.
+    /// </summary>
+	[Key] 
+	public int Id { get; set; } 
+	
+	/// <summary>
+    /// The title of the book.
+    /// </summary>
+    [Required]
+    public string Title { get; set; }
 
-        [Required] 
-        public string Title { get; set; } 
+    /// <summary>
+    /// The author of the book.
+    /// </summary>
+    [Required]
+    public string Author { get; set; }
 
-        [Required] 
-        public string Author { get; set; } 
+    /// <summary>
+    /// The publisher of the book.
+    /// </summary>
+    [Required]
+    public string Publisher { get; set; }
 
-        [Required] 
-        public string Publisher { get; set; } 
+    /// <summary>
+    /// The date when the book was published.
+    /// </summary>
+    [Required]
+    public DateTime PublicationDate { get; set; }
 
-        [Required] 
-        public DateTime PublicationDate { get; set; } 
+    /// <summary>
+    /// The ISBN number of the book.
+    /// </summary>
+    [Required]
+    public string ISBN { get; set; }
 
-        [Required] 
-        public string ISBN { get; set; } 
-
-        public string Edition { get; set; } 
-
-        [Required] 
-        public string Language { get; set; } 
+    /// <summary>
+    /// The language the book is written in.
+    /// </summary>
+    [Required]
+    public string Language { get; set; }
 
 
      /*   public BookModel(int id, string title, string author, string publisher, DateTime publicationDate, string isbn, string edition, string language)

66f955cf9e364d2d43d6190db4050c876de86797
ferskken <andreashavnen@gmail.com>
2024-10-18 10:33:24 +0200

    Removed vue startup project boilerplate code

--- frontend-vue/src/router/index.ts
+++ frontend-vue/src/router/index.ts
@@ -1,5 +1,4 @@
 import { createRouter, createWebHistory } from 'vue-router'
-// import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -12,15 +11,12 @@ const router = createRouter({
     {
       path: '/bookList',
       name: 'bookList',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: () => import('../views/BookList.vue'),
     },
     {
       path: '/bookSearch',
       name: 'bookSearch',
-      component: () => import('../components/BookSearch.vue'),
+      component: () => import('../components/BookSearchId.vue'),
     },
     {
       path: '/bookDelete',
